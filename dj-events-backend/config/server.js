74058983c2cfcaad24fff8b73511568d9fb86e66
Alex M <1maltsevalex@gmail.com>
2021-09-01 08:14:43 +0300

    014->Strapi CMS Setup

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3025db4824eaa9349057ff56449599b4'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '30119426-c29a-4a45-8d14-40f78d456319'
+};
