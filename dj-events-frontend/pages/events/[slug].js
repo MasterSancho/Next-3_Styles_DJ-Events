63432e964238db06898c3f63f8268381d6a60bbb
Alex M <1maltsevalex@gmail.com>
2021-09-08 10:25:18 +0300

    041->Authenticated Edit, Delete & Image Upload

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -11,22 +11,6 @@ import { useRouter } from 'next/router';
 export default function EventPage({ evt }) {
  const router = useRouter();
 
- const deleteEvent = async (e) => {
-  if (confirm('Are you sure?')) {
-   const res = await fetch(`${API_URL}/events/${evt.id}`, {
-    method: 'DELETE',
-   });
-
-   const data = await res.json();
-
-   if (!res.ok) {
-    toast.error(data.message);
-   } else {
-    router.push('/events');
-   }
-  }
- };
-
  return (
   <Layout>
    <div className={styles.event}>
@@ -36,9 +20,6 @@ export default function EventPage({ evt }) {
        <FaPencilAlt /> Edit Event
       </a>
      </Link>
-     <a href='#' className={styles.delete} onClick={deleteEvent}>
-      <FaTimes /> Delete Event
-     </a>
     </div>
 
     <span>
